0fbd937218143905f8df28bb324f389d54948c1b
Romain <rmtestard@gmail.com>
2019-01-13 15:46:17 +0100

    fetching data from Search component

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -6,6 +6,28 @@ class Search extends Component {
   state = {
     trackTitle: ''
   };
+
+  findTrack = e => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${
+          this.state.trackTitle
+        }&page_size=10&page=1&s_track_rating=desc&apikey=${
+          process.env.REACT_APP_MM_KEY
+        }`
+      )
+      .then(res => {
+        console.log(res.data);
+        this.setState({ track_list: res.data.message.body.track_list });
+      })
+      .catch(err => console.log(err));
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <Consumer>
@@ -16,6 +38,24 @@ class Search extends Component {
                 <i className="fas fa-music" /> Search For A Song
               </h1>
               <p className="lead text-center">Get the lyrics for any song</p>
+              <form onSubmit={this.findTrack}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Song title..."
+                    name="trackTitle"
+                    value={this.state.trackTitle}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <button
+                  className="btn btn-primary btn-lg btn-block mb-5"
+                  type="submit"
+                >
+                  Get Track Lyrics
+                </button>
+              </form>
             </div>
           );
         }}
