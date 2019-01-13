a9e61b552d36957a14b9982c2bd45b426c63031b
Romain <rmtestard@gmail.com>
2019-01-13 11:48:54 +0100

    components architecture done

--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Tracks from '../tracks/Tracks';
 
 const Index = () => {
-  return <div />;
+  return (
+    <>
+      <Tracks />
+    </>
+  );
 };
 
 export default Index;
